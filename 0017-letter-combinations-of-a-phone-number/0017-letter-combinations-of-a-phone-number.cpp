a9a167617f75a19bc614f3ba162219d7e50ccb75
Ritesh Agrahari <60784540+ritesh25033@users.noreply.github.com>
2023-08-03 13:53:33 +0530

    Time: 3 ms (37.56%), Space: 6.6 MB (31.4%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> rec(string &digits,int i,map<int,string> &mp){
+        if(i==digits.size())
+            return {""};
+        vector<string> temp=rec(digits,i+1,mp);
+        vector<string> ans;
+        for(auto it1:mp[digits[i]-'0']){
+            for(auto it2:temp){
+                ans.push_back(it1+it2);
+            }
+        }
+        return ans;
+    }
+    vector<string> letterCombinations(string digits) {
+        map<int,string> mp;
+        mp[2]="abc";
+        mp[3]="def";
+        mp[4]="ghi";
+        mp[5]="jkl";
+        mp[6]="mno";
+        mp[7]="pqrs";
+        mp[8]="tuv";
+        mp[9]="wxyz";
+        if(digits.size()==0)return {};
+        return rec(digits,0,mp);
+    }
+};
