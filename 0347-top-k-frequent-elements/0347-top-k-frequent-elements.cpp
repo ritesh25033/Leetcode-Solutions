02affb08d6a5f01555c38ab35986a7315f78428d
Ritesh Agrahari <60784540+ritesh25033@users.noreply.github.com>
2023-09-15 17:40:09 +0530

    Time: 12 ms (69.48%), Space: 14.2 MB (15.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<pair<int,int>> ans;
         vector<int> res;
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
