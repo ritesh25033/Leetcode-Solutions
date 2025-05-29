b30ba7ca6e143f2ddb8a748d428440b7523031a8
Ritesh Agrahari <60784540+ritesh25033@users.noreply.github.com>
2025-05-29 18:00:08 +0530

    Time: 9 ms (86.36%), Space: 57.9 MB (57.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,8 +4,20 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    console.log(s.split(''));
-    const s1=s.split('').sort().join();
-    const t1=t.split('').sort().join();
-    return s1===t1;
+    // console.log(s.split(''));
+    // const s1=s.split('').sort().join();
+    // const t1=t.split('').sort().join();
+    // return s1===t1;
+    if(s.length!=t.length)return false;
+    const mp = new Map();
+    for(let i=0;i<s.length;i++){
+        let n=mp.get(s[i])||0;
+        mp.set(s[i],n+1);
+        let m=mp.get(t[i])||0;
+        mp.set(t[i],m-1);
+    }
+    for(const [key,value] of mp){
+        if(value==0)continue;else return false;
+    }
+     return true;
 };
